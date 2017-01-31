16c6d181b083e08429771068dcbdb0090b86c2c4
AmarSadikovic <amar.sadikovic@hotmail.com>
2017-01-31 10:58:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/amar/project2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.amar.project2;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+public class MainActivity extends Activity implements SensorEventListener{
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
